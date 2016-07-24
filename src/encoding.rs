c1fd828a7d920cf3b80211484af89713da35ba12
Scott Schroeder <scott19904@gmail.com>
2016-07-23 21:39:45 -0700

    moved encoding logic to dedicated structure

---
+++ src/encoding.rs
@@ -0,0 +1,155 @@
+#[macro_use] use custom_derive;
+#[macro_use] use newtype_derive;
+
+use std::collections::HashMap;
+use std::fmt;
+
+#[derive(Debug)]
+pub enum Action {
+    Encrypt,
+    Decrypt,
+}
+
+custom_derive! {
+    #[derive(Clone, Copy, PartialEq, PartialOrd, Eq, Ord, Debug, Hash, NewtypeFrom, NewtypeAdd, NewtypeSub, NewtypeRem)]
+    pub struct EncodeNum(u64);
+}
+
+impl fmt::Display for EncodeNum {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+pub fn transform(message: &EncodeNum, key: &EncodeNum, size: &usize, action: &Action) -> EncodeNum {
+    let s = *size as i32;
+    let m = message.0 as i32;
+    let k = key.0 as i32;
+    let c = match *action {
+        Action::Encrypt => m + k,
+        Action::Decrypt => m - k,
+    };
+    let cipher_num = if c < 0 {
+        c + s
+    } else if c >= s {
+        c % s
+    } else {
+        c
+    };
+
+    EncodeNum(cipher_num as u64)
+}
+
+#[derive(Debug)]
+pub struct Encoding {
+    char_number_map: HashMap<char, EncodeNum>,
+    number_char_map: HashMap<EncodeNum, char>,
+    char_char_map: HashMap<char, char>,
+    size: usize,
+}
+
+impl Encoding {
+
+    pub fn new() -> Encoding {
+        Encoding {
+            char_number_map: HashMap::new(),
+            number_char_map: HashMap::new(),
+            char_char_map: HashMap::new(),
+            size: 0
+        }
+    }
+
+    pub fn len(&self) -> usize {
+        self.size
+    }
+
+    pub fn insert_char(&mut self, c: char) {
+        let map_number = EncodeNum(self.size as u64);
+        self.char_number_map.insert(c, map_number);
+        self.number_char_map.insert(map_number, c);
+        self.size = self.size + 1;
+    }
+
+    pub fn insert_map(&mut self, x: char, y: char) {
+        self.char_char_map.insert(x, y);
+    }
+
+    pub fn char_in_working_set(&self, c: &char) -> bool {
+        self.char_number_map.contains_key(c)
+    }
+
+    pub fn char_to_number(&self, c: &char) -> EncodeNum {
+        *self.char_number_map.get(c).unwrap()
+    }
+
+    pub fn number_to_char(&self, n: &EncodeNum) -> char {
+        *self.number_char_map.get(n).unwrap()
+    }
+
+    pub fn map_char(&self, c: &char) -> char {
+        *self.char_char_map.get(c).unwrap_or(c)
+    }
+
+}
+
+
+pub fn short_abc() -> Encoding {
+    let mut e = Encoding::new();
+    e.insert_char('a');
+    e.insert_char('b');
+    e.insert_char('c');
+    e.insert_map('A', 'a');
+    e.insert_map('B', 'b');
+    e.insert_map('C', 'c');
+    e
+}
+
+#[test]
+fn create_empty_encoding() {
+    let mut e = Encoding::new();
+    assert_eq!(e.size, 0);
+    assert_eq!(e.char_number_map.len(), 0);
+    assert_eq!(e.number_char_map.len(), 0);
+    assert_eq!(e.char_char_map.len(), 0);
+}
+
+#[test]
+fn insert_new_char() {
+    let mut e = Encoding::new();
+    e.insert_char('a');
+    assert_eq!(e.size, 1);
+    assert_eq!(e.char_number_map.len(), 1);
+    assert_eq!(e.number_char_map.len(), 1);
+    assert_eq!(e.char_char_map.len(), 0);
+}
+
+#[test]
+fn insert_new_map() {
+    let mut e = Encoding::new();
+    e.insert_map('A', 'a');
+    assert_eq!(e.size, 0);
+    assert_eq!(e.char_number_map.len(), 0);
+    assert_eq!(e.number_char_map.len(), 0);
+    assert_eq!(e.char_char_map.len(), 1);
+}
+
+#[test]
+fn translate_with_map() {
+    let mut e = Encoding::new();
+    e.insert_map('A', 'a');
+    assert_eq!(e.map_char(&'A'), 'a')
+}
+
+#[test]
+fn encode() {
+    let mut e = Encoding::new();
+    e.insert_char('a');
+    assert_eq!(e.char_to_number(&'a'), EncodeNum(0))
+}
+
+#[test]
+fn decode() {
+    let mut e = Encoding::new();
+    e.insert_char('a');
+    assert_eq!(e.number_to_char(&EncodeNum(0)), 'a')
+}
--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,41 @@
-#[macro_use]
-extern crate log;
+#[macro_use] extern crate log;
+#[macro_use] extern crate custom_derive;
+#[macro_use] extern crate newtype_derive;
 extern crate env_logger;
 extern crate itertools;
 use itertools::Itertools;
+mod encoding;
+use encoding::{Action, Encoding, EncodeNum, transform};
 
-const LETTERS: u8 = 26;
-const LOWERCASE: u8 = 'a' as u8 - 'A' as u8;
 
-enum Action {
-    Encrypt,
-    Decrypt,
-}
 
-fn char_to_number(c: &char) -> u8 {
-    *c as u8 - 'a' as u8
-}
 
-fn number_to_char(n: &u8) -> char {
-    (*n % LETTERS + 'a' as u8) as char
-}
 
-fn char_in_working_set(c: &char) -> bool {
-    match *c {
-        'a'...'z' => true,
-        _ => false,
-    }
-}
-
-fn map_char(c: &char) -> char {
-    match *c {
-        'A'...'Z' => (*c as u8 + LOWERCASE) as char,
-        _ => *c,
-    }
-}
-
-fn reduce_string(s: &String) -> String {
+fn reduce_string(s: &String, encoding: &Encoding) -> String {
     s.chars()
-        .map(|c| map_char(&c))
-        .filter(|c| char_in_working_set(&c))
+        .map(|c| encoding.map_char(&c))
+        .filter(|c| encoding.char_in_working_set(&c))
         .join("")
 }
 
-fn vectorize_string(s: &String) -> Vec<u8> {
+fn vectorize_string(s: &String, encoding: &Encoding) -> Vec<EncodeNum> {
     s.chars()
-        .map(|c| char_to_number(&c))
+        .map(|c| encoding.char_to_number(&c))
         .collect()
 }
 
 
-fn transform_message(message: &String, keytext: &String, action: Action) -> String {
-    let key: Vec<u8> = vectorize_string(keytext);
+fn transform_message(message: &String, keytext: &String, action: Action, encoding: &Encoding) -> String {
+    let key: Vec<EncodeNum> = vectorize_string(keytext, encoding);
     let keysize = key.len();
-    let direction: i32 = match action {
-        Action::Encrypt => 1,
-        Action::Decrypt => -1,
-    };
     message.chars()
         .enumerate()
         .map(|(i, c)| {
-            let m = char_to_number(&c);
-            let mut shift = key[i % keysize] as i32 * direction;
-            if shift < 0 {
-                shift += LETTERS as i32
-            }
-            let x = m + shift as u8;
-            let c = number_to_char(&x);
-            trace!("{}: {} +/- {} = {}", i, m, shift, c);
-            c
+            let message_num: EncodeNum = encoding.char_to_number(&c);
+            let key_num: EncodeNum = key[i % keysize];
+            let cipher_num = transform(&message_num, &key_num, &encoding.len(), &action);
+            trace!("message_num: {:?} key_num: {:?} {:?} -> {:?}", message_num, key_num, action, cipher_num);
+            encoding.number_to_char(&cipher_num)
         })
         .join("")
 }
@@ -77,19 +46,19 @@ fn main() {
 
     info!("starting up");
 
-    let s: String = "abc Hello, World! xyz".to_string();
-    let k: String = "This is my key".to_string();
-    println!("Raw Message: {}", s);
-    println!("Raw Keytext: {}", k);
-    let message = reduce_string(&s);
-    let keytext = reduce_string(&k);
-    println!("Message: {}", message);
-    println!("Keytext: {}", keytext);
-    let ciphertext = transform_message(&message, &keytext, Action::Encrypt);
-    println!("Ciphertext {}", ciphertext);
-    let plaintext = transform_message(&ciphertext, &keytext, Action::Decrypt);
-    println!("Plaintext {}", plaintext);
+    let encoding = encoding::short_abc();
+    println!("{:?}", encoding);
 
+    let s: String = "AaBbCc".to_string();
+    let k: String = "cab".to_string();
+    let message = reduce_string(&s, &encoding);
+    let keytext = reduce_string(&k, &encoding);
+    println!("Message {}", message);
+    println!("Key {}", keytext);
+    let ciphertext = transform_message(&message, &keytext, Action::Encrypt, &encoding);
+    println!("Ciphertext {}", ciphertext);
+    let plaintext = transform_message(&ciphertext, &keytext, Action::Decrypt, &encoding);
+    println!("Plaintext {}", plaintext);
 
 }
 
@@ -99,11 +68,12 @@ fn it_works() {}
 
 #[test]
 fn end_to_end() {
+    let encoding = encoding::short_abc();
     let s: String = "abc Hello, World! xyz".to_string();
-    let k: String = "This is my key".to_string();
-    let message = reduce_string(&s);
-    let keytext = reduce_string(&k);
-    let ciphertext = transform_message(&message, &keytext, Action::Encrypt);
-    let plaintext = transform_message(&ciphertext, &keytext, Action::Decrypt);
+    let k: String = "This is a key".to_string();
+    let message = reduce_string(&s, &encoding);
+    let keytext = reduce_string(&k, &encoding);
+    let ciphertext = transform_message(&message, &keytext, Action::Encrypt, &encoding);
+    let plaintext = transform_message(&ciphertext, &keytext, Action::Decrypt, &encoding);
     assert_eq!(message, plaintext);
 }
